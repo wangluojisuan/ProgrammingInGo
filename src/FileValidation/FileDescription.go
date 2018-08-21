78c136943fd2d91be7d6347538af1796e85721be
WangJ <wangluojisuan@163.com>
2018-08-21 22:45:18 +0800

    添加文件信息包。

---
+++ src/FileValidation/FileDescription.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    "github.com/gonutz/w32"
+)
+
+func main() {
+    const path = `cmd.exe`
+
+    size := w32.GetFileVersionInfoSize(path)
+    if size <= 0 {
+        panic("GetFileVersionInfoSize failed")
+    }
+
+    info := make([]byte, size)
+    ok := w32.GetFileVersionInfo(path, info)
+    if !ok {
+        panic("GetFileVersionInfo failed")
+    }
+
+    fixed, ok := w32.VerQueryValueRoot(info)
+    if !ok {
+        panic("VerQueryValueRoot failed")
+    }
+    version := fixed.FileVersion()
+    fmt.Printf(
+        "file version: %d.%d.%d.%d\n",
+        version&0xFFFF000000000000>>48,
+        version&0x0000FFFF00000000>>32,
+        version&0x00000000FFFF0000>>16,
+        version&0x000000000000FFFF>>0,
+    )
+
+    translations, ok := w32.VerQueryValueTranslations(info)
+    if !ok {
+        panic("VerQueryValueTranslations failed")
+    }
+    if len(translations) == 0 {
+        panic("no translation found")
+    }
+    fmt.Println("translations:", translations)
+
+    t := translations[0]
+    // w32.CompanyName simply translates to "CompanyName"
+    company, ok := w32.VerQueryValueString(info, t, w32.CompanyName)
+    if !ok {
+        panic("cannot get company name")
+    }
+    fmt.Println("company:", company)
+}
--- src/FileValidation/FileValidation.go
+++ src/FileValidation/FileValidation.go
@@ -6,19 +6,22 @@ import (
 )
 
 func main() {
-    xlsx, err := excelize.OpenFile("test.xlsx")
+	fileInfo, _ := os.Stat("1.xlsx")
+    xlsx, err := excelize.OpenFile("1.xlsx")
     if err != nil {
         fmt.Println(err)
         return
     }
-    cell := xlsx.GetCellValue("Sheet1", "B2")
+    cell := xlsx.GetCellValue("ATS系统发布项信息表", "B2")
     fmt.Println(cell)
 
-    rows := xlsx.GetRows("Sheet1")
+    rows := xlsx.GetRows("ATS系统发布项信息表")
     for _, row := range rows {
         for _, colCell := range row {
             fmt.Print(colCell, "\t")
         }
         fmt.Println()
     }
+	
+	fmt.Scan()
 }
